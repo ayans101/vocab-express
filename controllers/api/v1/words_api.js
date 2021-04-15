849dbf6433273adb81a98a14b660b7c7e8ea8ebe
Ayan Sarkar <ayansarkar367@gmail.com>
2021-04-15 17:15:43 +0530

    create API to respond with words searched by name

--- controllers/api/v1/words_api.js
+++ controllers/api/v1/words_api.js
@@ -12,3 +12,29 @@ module.exports.index = async function(req, res){
         }
     });
 }
+
+module.exports.searchWords = async function(req, res){
+    try{
+        // console.log(req.query);
+        // { text: 'abc' }
+        
+        await Word.find({ name: { $regex : new RegExp(req.query.text, "i") } }, (err, words) => {
+            
+            return res.json(200, {
+                message: "Request successful!",
+                success: true,
+                data: {
+                    words: words
+                }
+            });
+            
+        });
+
+    }catch(err){
+        console.log(err);
+        return res.json(500, {
+            message: "Internal Server Error",
+            success: false
+        });
+    }
+}
--- routes/api/v1/words.js
+++ routes/api/v1/words.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const wordsApi = require('../../../controllers/api/v1/words_api');
 
 router.get('/', wordsApi.index);
+router.post('/search', wordsApi.searchWords);
 
 module.exports = router;
