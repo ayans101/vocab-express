5cc542391c6714d17170b6b6917a11a57e6458c3
Ayan Sarkar <ayansarkar367@gmail.com>
2021-04-15 07:06:51 +0530

    add controllers for api

---
+++ controllers/api/v1/words_api.js
@@ -0,0 +1,5 @@
+const Word = require('../../../models/word');
+
+module.exports.index = async function(req, res){
+    return;
+}
--- routes/api/v1/words.js
+++ routes/api/v1/words.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const router = express.Router();
+const wordsApi = require('../../../controllers/api/v1/words_api');
 
+router.get('/', wordsApi.index);
 
 module.exports = router;
